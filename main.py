76feb39223e8edf02a2f2d56ee32ed79c1390d20
Mohammed Nagdy <119080152+monagdyALX@users.noreply.github.com>
2022-11-30 17:01:39 +0000

    Added Circleci yaml

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from threading import Thread
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return b'Hello from Flask!'
 
+def run():
+  app.run(host='0.0.0.0', port=80)
 
-app.run(host='0.0.0.0', port=81)
+t = Thread(target=run)
+t.start()
